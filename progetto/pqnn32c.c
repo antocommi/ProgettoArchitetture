0617b64cec418df447053ce9d3c6908184eac997
iAlex5 <alessandrobarbieri97@yahoo.it>
2019-05-05 19:35:01 +0200

    aggiunta distanza asimmetrica

--- progetto/pqnn32c.c
+++ progetto/pqnn32c.c
@@ -203,7 +203,7 @@ int calcolaQ(params* input, int x){
     //	OUTPUT: Centroide c più vicino ad x. 
     //
     int i;
-    double min= 2.3E-308;
+    double min=1.79E+308;
     int imin=-1;
     double temp;
 
@@ -217,12 +217,27 @@ int calcolaQ(params* input, int x){
     return imin;
 }
 
+double dist_simmetrica(params* input, int punto1, int punto2){
+	int i;
+	double ret=0;
+	for(i=0; i<input->d; i++){
+		ret += pow( input->codebook[punto1*input->d+i] - input->codebook[punto2*input->d+i] , 2);
+	}
+	return ret;
+}
 
-int dist(params* input, int punto1, int punto2){
+double dist_asimmetrica(params* input, int punto1, int punto2){
+	int i;
+	double ret=0;
+	for(i=0; i<input->d; i++){
+		ret += pow( input->ds[punto1*input->d+i] - input->codebook[input->q[punto2]*input->d+i] , 2);
+	}
+	return ret;
+}
+
+double dist(params* input, int punto1, int punto2){
 	if(input->symmetric==0){
-		//Distanza Asimmetrica
-		// TODO
-		return -1;
+		return dist_asimmetrica(inpit, punto1, punto2);
 	}else{
 		if(punto1==punto2){
 			return 0;
@@ -233,17 +248,6 @@ int dist(params* input, int punto1, int punto2){
 		}
 	}
 	return -1;
-}
-
-
-
-double dist_simmetrica(params* input, int punto1, int punto2){
-	int i;
-	double ret=0;
-	for(i=0; i<input->d; i++){
-		ret += pow( input->codebook[punto1*input->d+i] - input->codebook[punto2*input->d+i] , 2);
-	}
-	return ret;
 }
 
 void kmeans(params* input, int start, int end){
